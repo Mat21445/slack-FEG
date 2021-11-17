51f084cb4d9a70177f6c24d492fba68ace6e049b
Mateusz Ostrówka <92634025+Mat21445@users.noreply.github.com>
2021-11-17 20:02:56 +0100

    Add files via upload
    Code refactored a bit

--- main.go
+++ main.go
@@ -42,29 +42,31 @@ func main() {
 
 	// time loop
 	for _ = range time.Tick(time.Hour) {
+		CheckAndSend(url, TmpTime)
 
-		// Download and unmarshal JSON information
-		var web_data tmpData = fetch()
-
-		// loop that checks if the game is currently free and not posted yet
-		for i := 0; i < len(web_data.Data.Catalog.SearchStore.Elements); i++ {
-			if web_data.Data.Catalog.SearchStore.Elements[i].EffectiveDate < time.Now().UTC().Format("2006-01-02T15:04:05.000Z") && web_data.Data.Catalog.SearchStore.Elements[i].EffectiveDate > TmpTime {
-
-				// send the message using Slack webhook
-				attachment1 := slack.Attachment{}
-				attachment1.AddField(slack.Field{Title: "New Free Epic Game Available!:", Value: web_data.Data.Catalog.SearchStore.Elements[i].Title}).AddField(slack.Field{Title: "Description:", Value: web_data.Data.Catalog.SearchStore.Elements[i].Title})
-				attachment1.AddAction(slack.Action{Type: "button", Text: "Check it!", Url: "https://www.epicgames.com/store/en-US/free-games", Style: "primary"})
-				payload := slack.Payload{
-					Attachments: []slack.Attachment{attachment1},
-				}
-				err := slack.Send(*url, "", payload)
-				if len(err) > 0 {
-					fmt.Printf("error: %s\n", err)
-				}
-			}
-		}
 		// Change the TmpTime variable
 		*p = time.Now().UTC().Format("2006-01-02T15:04:05.000Z")
+	}
+}
+
+func CheckAndSend(url *string, time1 string) {
+	// Download and unmarshal JSON information
+	var web_data tmpData = fetch()
+
+	// loop that checks if the game is currently free and not posted yet
+	for i := 0; i < len(web_data.Data.Catalog.SearchStore.Elements); i++ {
+		if web_data.Data.Catalog.SearchStore.Elements[i].EffectiveDate < time.Now().UTC().Format("2006-01-02T15:04:05.000Z") && web_data.Data.Catalog.SearchStore.Elements[i].EffectiveDate > time1 {
+
+			// send the message using Slack webhook
+			attachment1 := CreateFEGAttachment(web_data.Data.Catalog.SearchStore.Elements[i].Title, web_data.Data.Catalog.SearchStore.Elements[i].Description)
+			payload := slack.Payload{
+				Attachments: []slack.Attachment{attachment1},
+			}
+			err := slack.Send(*url, "", payload)
+			if len(err) > 0 {
+				fmt.Printf("error: %s\n", err)
+			}
+		}
 	}
 }
 
@@ -87,4 +89,13 @@ func fetch() tmpData {
 	}
 
 	return p1
+}
+
+// function that creates the attachment
+func CreateFEGAttachment(title string, description string) (attachment slack.Attachment) {
+
+	attachment.AddField(slack.Field{Title: "New Free Epic Game Available!:", Value: title}).AddField(slack.Field{Title: "Description:", Value: description})
+	attachment.AddAction(slack.Action{Type: "button", Text: "Check it!", Url: "https://www.epicgames.com/store/en-US/free-games", Style: "primary"})
+
+	return attachment
 }
